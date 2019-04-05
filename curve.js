5299491e21e961bb62587012701409ace67c9478
Cyber Witch <no-reply>
2019-04-04 21:52:14 -0600

    final draft

--- curve.js
+++ curve.js
@@ -3,8 +3,8 @@ var canvas = document.getElementById("dc-canvas")
 var draw = canvas.getContext("2d");
 
 var foldingPoint = {
-    xPos: 500,
-    yPos: 300
+    xPos: 0,
+    yPos: 0
 }
 
 var nextFoldingPoint;
@@ -13,7 +13,7 @@ var unfoldInter;
 
 var foldingNum = 1;
 
-var lineLength = 7;
+var lineLength = 10;
 
 var points = [];
 
@@ -21,11 +21,17 @@ var inAni = false;
 
 var counter = 0;
 
+var scale = 20;
+
+var zoom = .9965;
+
+var unfoldCount = 0
+
 //prototype for points on the dragon curve
 function Point(x,y) {
-    this.xPos = x + 500;
+    this.xPos = x;
 
-    this.yPos = y + 300;
+    this.yPos = y;
 
     this.yPosRel = 0;
 
@@ -39,20 +45,29 @@ function Point(x,y) {
 };
 
 //first points of the first iteration.
-points.push(new Point(1 * lineLength, 1 * lineLength));
+points.push(new Point(0 * lineLength, 1 * lineLength));
 
 points.push(new Point(0,0));
 
 console.log(points)
 
+draw.lineWidth = "2";
+draw.strokeStyle = "#88bb00"; // Green path
+draw.lineJoin = "round"
+draw.translate(500,350)
+draw.scale(scale,scale);
+draw.lineCap = "round"
 //function to render line
 
 function render(){
-    draw.clearRect(0,0,1000,1000)
 
+
+    draw.clearRect(-5000,-5000,10000,10000)
+
+    
+    
     draw.beginPath();
-    draw.lineWidth = "2";
-    draw.strokeStyle = "#88bb00"; // Green path
+    
 
     draw.moveTo(points[0].xPos,points[0].yPos);
 
@@ -66,10 +81,12 @@ render()
 
 $("#unfold").click(function(){
     if(!inAni){
+        counter = 0;
+
         var currentPoints = points.length - 2;
 
         for(i = currentPoints; i > -1; i--){
-            var newPoint = new Point(points[i].xPos-500,points[i].yPos-300);
+            var newPoint = new Point(points[i].xPos,points[i].yPos);
 
 
             //setting x and y positions relative to the folding pivot point.
@@ -95,6 +112,24 @@ $("#unfold").click(function(){
         },20);
 
         inAni = true;
+
+        $(".btn-large").addClass("disabled btn-flat")
+    }
+})
+
+$("#fold").click(function(){
+    if(!inAni){
+        counter = 90;
+
+        foldInter = setInterval(function(){
+            foldAni()
+        },20);
+
+        inAni = true;
+
+        foldingPoint = points[points.length/2 -.5]
+
+        $(".btn-large").addClass("disabled btn-flat")
     }
 })
 
@@ -110,10 +145,14 @@ function unfoldAni(){
         pt.xPos = pt.length * Math.sin(pt.angle - degToRad(counter)) + foldingPoint.xPos
         pt.yPos = pt.length * Math.cos(pt.angle - degToRad(counter)) + foldingPoint.yPos
 
-        render();
     }
 
-    
+    if(unfoldCount === 0)
+        draw.rotate(degToRad(-.5))
+
+    draw.scale(zoom,zoom)
+
+    render()
 
 
     if(counter === 90){
@@ -124,6 +163,55 @@ function unfoldAni(){
         inAni = false;
 
         foldingPoint = nextFoldingPoint;
+
+        unfoldCount++
+
+        $("#unfold-count").text(unfoldCount)
+
+        $(".btn-large").removeClass("disabled btn-flat")
+
+        if(unfoldCount > 13)
+            $("#warning").css("display","block")
+    }
+}
+
+//folding animation
+function foldAni(){
+    counter--
+
+    for( let i = points.length/2 + .5; i < points.length; i++){
+
+        var pt = points[i];
+
+        pt.xPos = pt.length * Math.sin(pt.angle - degToRad(counter)) + foldingPoint.xPos
+        pt.yPos = pt.length * Math.cos(pt.angle - degToRad(counter)) + foldingPoint.yPos
+
+    }
+
+    if(unfoldCount === 1)
+        draw.rotate(degToRad(+.5))
+
+    draw.scale(1/zoom,1/zoom)
+
+    render()
+
+    if(counter === 0){
+        clearInterval(foldInter);
+
+        inAni = false;
+
+        points = points.slice(0,points.length/2 + .5)
+
+        foldingPoint = points[points.length - 1];
+
+        unfoldCount--
+
+        $("#unfold-count").text(unfoldCount)
+
+        $(".btn-large").removeClass("disabled btn-flat")
+
+        if(unfoldCount < 14)
+            $("#warning").css("display","none")
     }
 }
 
